618ad8f3d323159208157a5c881813d2a9096123
zhen <zhangsan@itcast.cn>
2019-01-07 21:22:33 +0800

    将本地工程git_test_project添加到git

---
+++ git_test_project/src/com/itcast/git/FirstClass.java
@@ -0,0 +1,27 @@
+package com.itcast.git;
+
+/**
+ * @创建人 yang
+ * @创建时间 2019/1/7
+ * @描述
+ */
+public class FirstClass {
+    public static void main(String[] args){
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
